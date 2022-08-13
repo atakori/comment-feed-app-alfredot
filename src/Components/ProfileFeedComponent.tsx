5c0f0755b2d09f5c78dfc8c4e2917ba1a2b1d27f
Alfredo T <alfredo.takori@revelsystems.com>
2022-08-13 00:32:44 -0400

    Adding TimeAgo dependency for timestamps

--- src/Components/ProfileFeedComponent.tsx
+++ src/Components/ProfileFeedComponent.tsx
@@ -1,7 +1,14 @@
 import React, { useContext, useEffect } from "react";
-import { PostsContextType } from "../types";
+import { PostsContextType, IUserPost } from "../types";
 import { PostsContext } from "../context/postsContext";
 import NoPostsFoundComponent from './NoPostsFoundComponent';
+import { PROFILEPHOTOURL } from "../constants";
+import TimeAgo from 'javascript-time-ago'
+import ReactTimeAgo from 'react-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
+
+// Adding locale Time for "Time Since Posted"
+TimeAgo.addDefaultLocale(en)
 
 const ProfileFeedComponent = () => {
     const { posts } = useContext(PostsContext) as PostsContextType;
@@ -12,9 +19,26 @@ const ProfileFeedComponent = () => {
 
     }, [posts])
 
+    function renderPosts(posts: IUserPost[]) {
+        return posts.map((post) => {
+            const {username, datePosted, message } = post
+            return (
+                <div> 
+                    <p>Picture goes here: {PROFILEPHOTOURL}</p>
+                    <p>{username}</p>
+                    <p>{message}</p>
+                    <ReactTimeAgo date={datePosted} locale="en-US"/>
+                </div>
+            )
+        })
+    }
+
     return posts.length === 0 ? <NoPostsFoundComponent/> : (
         <div>
             <h1> Posts will go here once there is a successful post!!!</h1>
+
+            {renderPosts(posts)}
+            <p>Add Comment Box</p>
         </div>
     );
   };
